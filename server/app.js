1b02ce94a068e9ce66a1b3fbc180ac99d8ab26fe
604karev <604karev@gmail.com>
2021-12-30 13:59:58 +0300

    fix: heroku setting fix

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema');
+const schema = require('../schema/schema');
 const mongoose = require('mongoose');
 const conrs = require('cors');
 
-const index = express();
+const app = express();
 const PORT = 3005;
 
 mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/GraphQL?retryWrites=true&w=majority', {
@@ -15,15 +15,15 @@ mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/Grap
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
 dbConnection.once('open', () => console.log('Connected to DB!'));
-index.use(conrs());
-index.use('/graphql', graphqlHTTP({
+app.use(conrs());
+app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
 
 
-index.listen(PORT, err => {
+app.listen(PORT, err => {
         err ? console.log(err) : console.log('Server started')
     }
 );

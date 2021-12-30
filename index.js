bfacb06a34539ba0f1b7fcc2ecbd28f7dee54866
604karev <604karev@gmail.com>
2021-12-30 13:46:08 +0300

    fix: heroku fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('../schema/schema');
+const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const conrs = require('cors');
 
-const app = express();
+const index = express();
 const PORT = 3005;
 
 mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/GraphQL?retryWrites=true&w=majority', {
@@ -15,15 +15,15 @@ mongoose.connect('mongodb+srv://test_user:test123@graphql.70erq.mongodb.net/Grap
 const dbConnection = mongoose.connection;
 dbConnection.on('error', err => console.log(`Connection error: ${err}`));
 dbConnection.once('open', () => console.log('Connected to DB!'));
-app.use(conrs());
-app.use('/graphql', graphqlHTTP({
+index.use(conrs());
+index.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
 
 
-app.listen(PORT, err => {
+index.listen(PORT, err => {
         err ? console.log(err) : console.log('Server started')
     }
 );
